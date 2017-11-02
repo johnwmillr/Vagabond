1bfbb9ef6dc225791a2e756034ef2d003acd8e64
John Miller <john-w-miller@uiowa.edu>
2017-11-02 21:37:12 +0000

    Add static map to places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,6 +4,13 @@ class PlacesController < ApplicationController
     end
 
     def show
+        # Load the static maps API key
+        file_lines = []
+        File.open('secrets.txt','r') do |f|
+            f.each_line {|line| file_lines = file_lines + [line.split(':')[1].strip()]}
+        end
+        @static_map_api_key = file_lines[1]
+
         @place = Place.find(params[:id])
     end
 
@@ -12,8 +19,10 @@ class PlacesController < ApplicationController
     end
 
     def create
-        @place = Place.new(place_params)
+        @place = Place.create_place!(place_params)
+        # @place = Place.new(place_params)
         if @place.save
+            # puts"@place.latitude: " + @place.latitude.inspect
             flash[:success] = 'Place added!'
             redirect_to root_path
         else
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,22 @@
 class Place < ApplicationRecord
 
-    # geocoded_by :address, latitude: :lat, longitude: :lon
-    geocoded_by :address
+    # geocoded_by :address, :longitude => :title
+    geocoded_by :address do |obj, results|
+        if geo = results.first
+            # puts '-------------------------------'
+            # puts obj.inspect
+            # puts '********'
+            # puts results.inspect
+            # puts [results[0].latitude, results[0].longitude].inspect
+            obj.latitude = geo.latitude
+            obj.longitude = geo.longitude
+        end
+    end
     after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+
+    def Place::create_place!(place_hash)
+        return new(place_hash)
+    end
+
 
 end
